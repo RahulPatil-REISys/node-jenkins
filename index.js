f60771cee1de97863a2d552691189249d4686f68
Rahul Patil <rahul.patil@reisystems.in>
2022-01-11 17:36:29 +0530

    adde new url

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 })
 
+app.get('/api/v1', (req, res) => {
+  res.send('Hello from /api/v1');
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 })
